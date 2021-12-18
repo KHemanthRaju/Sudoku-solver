3c90cf92024184f0be6f8b19c0093b4420a3defc
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2021-12-18 12:23:04 +0530

    feat : puzzle board is working

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const puzzleBoard = document.querySelector("#puzzle");
 const solveButton = document.querySelector("#solve-button");
 const squares = 81;
+const submission = [];
 
 for(let i=0;i<squares;i++){
     const inputElement = document.createElement('input');
     inputElement.setAttribute('type','number');
-    inputElement.setAttribute('min',0);
+    inputElement.setAttribute('min',1);
     inputElement.setAttribute('max',9);
     puzzleBoard.appendChild(inputElement);
 }
 
+const joinValues = () => {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(input => {
+        if(input.value){
+            submission.push(input.value);
+        }else{
+            submission.push('.');
+        }
+    })
+    console.log(submission);
+}
+
+const solve = () => {
+    
+}
+
+solveButton.addEventListener('click',joinValues);
